eda98d22e45469820d05e2ef8b80c6f9e7b76643
nicofreundt <nico.freundt@protonmail.com>
2021-01-27 12:27:12 +0100

    first commit

---
+++ precache-manifest.a40d09fb08d8b84f8880d89318886431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3a90c680024012740aa43666acdcf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6713dd024f1902ab72e",
+    "url": "/static/css/2.02f16f1f.chunk.css"
+  },
+  {
+    "revision": "49ccdfa6513acee9893d",
+    "url": "/static/css/main.c3e85f3a.chunk.css"
+  },
+  {
+    "revision": "a6713dd024f1902ab72e",
+    "url": "/static/js/2.13a3910a.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/static/js/2.13a3910a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ccdfa6513acee9893d",
+    "url": "/static/js/main.2aa084a0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
